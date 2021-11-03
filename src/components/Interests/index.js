438bfe53214dbc3d612965f8fc34bc2e8cbb2261
Ravindra <ravindra.kk@doodleblue.com>
2021-11-03 19:51:54 +0530

    SMall tag is added

--- src/components/Interests/index.js
+++ src/components/Interests/index.js
@@ -7,17 +7,17 @@ const Interest = () => {
     <NormalCard>
       <p>Interests</p>
       <NormalRow classNames={"flexWrap"}>
-        <div className="tag-1 small">News</div>
-        <div className="tag-2 small">W3Schools</div>
-        <div className="tag-3 small">Labels</div>
-        <div className="tag-4 small">Games</div>
-        <div className="tag-5 small">Friends</div>
-        <div className="tag-6 small">Games</div> 
-        <div className="tag-7 small">Friends</div>
-        <div className="tag-8 small">Food</div>
-        <div className="tag-9 small">Design</div> 
-        <div className="tag-10 small">Art</div>
-        <div className="tag-11 small">Photos</div>
+        <div className="tag-1 tag">News</div>
+        <div className="tag-2 tag">W3Schools</div>
+        <div className="tag-3 tag">Labels</div>
+        <div className="tag-4 tag">Games</div>
+        <div className="tag-5 tag">Friends</div>
+        <div className="tag-6 tag">Games</div> 
+        <div className="tag-7 tag">Friends</div>
+        <div className="tag-8 tag">Food</div>
+        <div className="tag-9 tag">Design</div> 
+        <div className="tag-10 tag">Art</div>
+        <div className="tag-11 tag">Photos</div>
       </NormalRow>
     </NormalCard>
   )
