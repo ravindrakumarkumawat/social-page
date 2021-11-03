da9eaa8f1a21ff23ab922d04698a390f1077954d
Ravindra <ravindra.kk@doodleblue.com>
2021-11-03 17:31:48 +0530

    added the three col

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,10 +1,17 @@
 import React from 'react'
+import NormalCol from '../common/NormalCol'
+import NormalRow from '../common/NormalRow'
 import HomeLayout from '../Layout/Home'
+import './Home.css'
 
 const Home = () => {
   return (
-    <HomeLayout>
-      <h1>name please</h1>
+    <HomeLayout>   
+      <NormalRow classNames="justifyBetween">
+        <NormalCol span={5}><div style={{border: "1px solid green"}}>1</div></NormalCol>
+        <NormalCol span={14}><div style={{border: "1px solid green"}}>1</div></NormalCol>
+        <NormalCol span={4}><div style={{border: "1px solid green"}}>1</div></NormalCol>
+      </NormalRow>
     </HomeLayout>
   )
 }
