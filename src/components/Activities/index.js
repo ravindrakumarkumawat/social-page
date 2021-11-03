ab9528d0c644e83c99aa93a444c943e31c48d12a
Ravindra <ravindra.kk@doodleblue.com>
2021-11-03 21:53:31 +0530

    added activities

--- src/components/Activities/index.js
+++ src/components/Activities/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import NormalCard from "../common/NormalCard";
 import NormalImg from "../common/NormalImg";
 import Lights from "../../assets/images/lights.jpg";
@@ -8,39 +8,54 @@ import Forest from "../../assets/images/forest.jpg";
 import Snow from "../../assets/images/snow.jpg";
 import NormalCol from "../common/NormalCol";
 import NormalRow from "../common/NormalRow";
+import './Activities.css'
 
 const Activities = () => {
+  const [isGroups, setIsGroups] = useState(false)
+  const [isEvents, setIsEvents] = useState(false)
+  const [isPhotos, setIsPhotos] = useState(false)
+
+  const toggleGroups = () => {
+    setIsGroups(!isGroups)
+  }
+
+  const toggleEvents = () => {
+    setIsEvents(!isEvents)
+  }
+  const togglePhotos = () => {
+    setIsPhotos(!isPhotos)    
+  }
   return (
     <NormalCard padding={false}>
       <div className="card round">
         <div className="white">
           <div
-            onClick="myFunction('Demo1')"
-            className="button block theme-l1 left-align tag-3"
+            onClick={toggleGroups}
+            className={`list-tag ${!isGroups ? '' : 'list-tag-active'} pointer`}
           >
             <i className="fa fa-circle-o-notch fa-fw margin-right"></i> My
             Groups
           </div>
-          <div id="Demo1" className="hide container">
+          <div className={`${!isGroups ? 'hide': ''} container`}>
             <p>Some text..</p>
           </div>
           <div
-            onClick="myFunction('Demo2')"
-            className="button block theme-l1 left-align tag-2"
+            onClick={toggleEvents}
+            className={`list-tag ${!isEvents ? '' : 'list-tag-active'} pointer`}
           >
             <i className="fa fa-calendar-check-o fa-fw margin-right"></i> My
             Events
           </div>
-          <div id="Demo2" className="hide container">
+          <div className={`${!isEvents ? 'hide': ''} container`}>
             <p>Some other text..</p>
           </div>
           <div
-            onClick="myFunction('Demo3')"
-            className="button block theme-l1 left-align tag-1"
+            onClick={togglePhotos}
+            className={`list-tag ${!isPhotos ? '' : 'list-tag-active'} pointer`}
           >
             <i className="fa fa-users fa-fw margin-right"></i> My Photos
           </div>
-          <div id="Demo3" className="hide container">
+          <div className={`${!isPhotos ? 'hide': ''} container`}>
             <NormalRow classNames={"flexWrap-between"}>
               <NormalCol span={44}>
                 <NormalImg src={Lights} />
