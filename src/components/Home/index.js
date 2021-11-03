e38cc975c9367557bd7aaf04620bb49876a7460e
Ravindra <ravindra.kk@doodleblue.com>
2021-11-03 19:21:09 +0530

    added the profile

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -7,12 +7,16 @@ import HomeLayout from '../Layout/Home'
 import FriendRequest from '../FriendRequest'
 import './Home.css'
 import UpcomingEvents from '../UpcomingEvents'
+import Profile from '../Profile'
 
 const Home = () => {
   return (
     <HomeLayout>   
       <NormalRow classNames="justifyBetween">
-        <NormalCol span={5}><div style={{border: "1px solid green"}}>1</div></NormalCol>
+        <NormalCol span={5}>
+          <Profile />
+          <div className="gap-2"></div>
+        </NormalCol>
         <NormalCol span={14}><div style={{border: "1px solid green"}}>1</div></NormalCol>
         <NormalCol span={4}>
           <UpcomingEvents />
---
+++ src/components/Profile/index.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import NormalCard from '../common/NormalCard'
+import NormalImg from '../common/NormalImg'
+import Avatar3 from '../../assets/images/avatar3.png'
+
+const Profile = () => {
+  return (
+    <NormalCard>
+    <div className="card round white">
+    <div className="container">
+      <div className="center">
+     <h4 className="center">My Profile</h4>
+     <NormalImg src={Avatar3} alt="Avatar" classNames="profile-img"/>
+  {/*<p className="center"><img src="/w3images/avatar3.png" className="circle" style="height:106px;width:106px" alt="Avatar"></p>*/}
+     </div>
+     <hr />
+     <p><i className="fa fa-pencil fa-fw margin-right text-theme"></i> Designer, UI</p>
+     <p><i className="fa fa-home fa-fw margin-right text-theme"></i> London, UK</p>
+     <p><i className="fa fa-birthday-cake fa-fw margin-right text-theme"></i> April 1, 1988</p>
+    </div>
+  </div>
+    </NormalCard>
+  )
+}
+
+export default Profile
