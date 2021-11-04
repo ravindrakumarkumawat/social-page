9c3813d31d1d6d9759fb4608d28fe671a279abb1
Ravindra <ravindra.kk@doodleblue.com>
2021-11-04 10:41:18 +0530

    added the bug

--- src/components/Ads/index.js
+++ src/components/Ads/index.js
@@ -4,7 +4,7 @@ import NormalCard from '../common/NormalCard'
 const Ads = () => {
   return (
     <NormalCard>
-      <div className="center">
+      <div className="center padding-16">
         <p>ADS</p>
       </div>
     </NormalCard>
